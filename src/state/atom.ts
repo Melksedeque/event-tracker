b86c4bba1d931577eb249c0d687ff8e261de38ce
Melksedeque <freelancer@melksedeque.com.br>
2025-05-21 19:27:51 -0300

    Atualização de data de tarefas de exemplo

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -6,15 +6,15 @@ export const listaDeEventosState = atom<IEvento[]>({
     default: [
         {
             "descricao": "Estudar React",
-            "inicio": new Date("2022-01-15T09:00"),
-            "fim": new Date("2022-01-15T13:00"),
+            "inicio": new Date("2025-05-22T09:00"),
+            "fim": new Date("2025-05-22T13:00"),
             "completo": false,
             "id": 1642342747
         },
         {
             "descricao": "Estudar Recoil",
-            "inicio": new Date("2022-01-16T09:00"),
-            "fim": new Date("2022-01-16T11:00"),
+            "inicio": new Date("2025-05-23T09:00"),
+            "fim": new Date("2025-05-23T11:00"),
             "completo": false,
             "id": 1642342959
         }
