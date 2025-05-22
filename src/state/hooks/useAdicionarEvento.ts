a217d6663b3efac82b2587674f31b8466a8231c5
Melksedeque <freelancer@melksedeque.com.br>
2025-05-22 18:36:54 -0300

    Criação de hook para criar evento; Validação de datas anteriores a atual

--- src/state/hooks/useAdicionarEvento.ts
+++ src/state/hooks/useAdicionarEvento.ts
@@ -1,9 +1,23 @@
 import { useSetRecoilState } from "recoil"
 import { IEvento } from "../../interfaces/IEvento"
 import { listaDeEventosState } from "../atom"
+import { generateId } from "../../util"
 
 const useAdicionarEvento = () => {
     const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState)
+
+    return (evento: IEvento) => {
+        const hoje = new Date()
+        hoje.setHours(0, 0, 0, 0)
+        const eventoEstaNoPassado = evento.inicio < hoje
+
+        if (eventoEstaNoPassado) {
+            throw new Error('Não é possível adicionar eventos no passado')
+        }
+
+        evento.id = generateId()
+        setListaDeEventos(listaAntiga => [...listaAntiga, evento])
+    }
 }
 
 export default useAdicionarEvento
