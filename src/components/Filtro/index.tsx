124d5c9656c8e1fa9bd12ac1ff43ca81fbff4303
Melksedeque <freelancer@melksedeque.com.br>
2025-05-23 18:15:27 -0300

    Substituição de aoFiltroAplicado pel setFiltroDeEvento usando o useSetRecoilState

--- src/components/Filtro/index.tsx
+++ src/components/Filtro/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import style from './Filtro.module.scss';
+import { useSetRecoilState } from 'recoil';
+import { filtroDeEventos } from '../../state/atom';
+import { IFiltroDeEventos } from '../../interfaces/IFiltroDeEventos';
 
-const Filtro: React.FC<{ aoFiltroAplicado: (data: Date | null) => void }> = ({
-  aoFiltroAplicado,
-}) => {
+const Filtro: React.FC = () => {
   const [data, setData] = useState('');
+  const setFiltroDeEvento = useSetRecoilState<IFiltroDeEventos>(filtroDeEventos);
 
   const submeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault();
